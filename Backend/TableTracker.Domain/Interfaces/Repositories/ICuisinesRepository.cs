4d5ae9e3bb2b259703beff8822d60e8de56cc4e9
Serhiy Pylypchuk <pylypchuk.serhiy@gmail.com>
2022-02-19 18:35:35 +0200

    Add CuisinesRepository

---
+++ Backend/TableTracker.Domain/Interfaces/Repositories/ICuisinesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+using TableTracker.Domain.Entities;
+using TableTracker.Domain.Enums;
+
+namespace TableTracker.Domain.Interfaces.Repositories
+{
+    public interface ICuisinesRepository : IRepository<Cuisines, long>
+    {
+        Task<Cuisines> FindCuisineByName(Cuisine cuisine);
+    }
+}
---
+++ Backend/TableTracker.Infrastructure/Repositories/CuisinesRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using TableTracker.Domain.Entities;
+using TableTracker.Domain.Enums;
+using TableTracker.Domain.Interfaces.Repositories;
+
+namespace TableTracker.Infrastructure.Repositories
+{
+    public class CuisinesRepository : Repository<Cuisines, long>, ICuisinesRepository
+    {
+        public CuisinesRepository(TableDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<Cuisines> FindCuisineByName(Cuisine cuisine)
+        {
+            return await _context
+                .Set<Cuisines>()
+                .FirstOrDefaultAsync(x => x.Cuisine == cuisine);
+        }
+    }
+}
